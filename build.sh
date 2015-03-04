172259450dde7974616622380948f7af08afb525
Simon Hildrew <simon@hildrew.net>
2015-03-04 14:44:13 +0000

    Derive BUILD_BRANCH when on TeamCity

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
+# try to get build branch
+if [ -n "${TEAMCITY_BUILD_PROPERTIES_FILE}" ]; then
+  CONFIG_FILE=$( grep "teamcity.configuration.properties.file=" ${TEAMCITY_BUILD_PROPERTIES_FILE} | cut -d'=' -f2 )
+  export BUILD_BRANCH=$( grep "teamcity.build.branch=" ${CONFIG_FILE} | cut -d'=' -f2 )
+fi
+
 pushd packer
 bash build.sh
 popd
