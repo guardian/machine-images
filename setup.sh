7360462559b0f258660634486adfc84ec4fd9262
Simon Hildrew <simon.hildrew@theguardian.com>
2015-04-19 20:09:14 +0100

    Check version and exit if already installed

--- setup.sh
+++ setup.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 # Install packer locally
+set -e
 
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+PACKER_DIR="${SCRIPTPATH}/packer_bin"
 
 PLATFORM=$(uname)
 PACKER_VERSION="0.7.5"
+
+if [ -x "${PACKER_DIR}/packer" ]; then
+  if ${PACKER_DIR}/packer version | grep -q "${PACKER_VERSION}"
+  then
+    echo "Packer ${PACKER_VERSION} already installed"
+    exit 0
+  else
+    echo "Packer installed, but not version ${PACKER_VERSION} - delete packer_bin directory and run again to install if desired"
+    exit 1
+  fi
+fi
 
 case "$PLATFORM" in
   Darwin)
@@ -15,14 +28,14 @@ case "$PLATFORM" in
     ;;
   *)
     echo "Unknown OS, please install packer yourself"
-    exit 1
+    exit 2
     ;;
 esac
 
 PACKER_FILE=$(mktemp /tmp/packer-XXXXXX)
 if [ $? -ne 0 ]; then
   echo "$0: can't create temp file for packer installation"
-  exit 2
+  exit 3
 fi
 
 trap "{ rm ${PACKER_FILE}; }" EXIT
@@ -30,10 +43,8 @@ trap "{ rm ${PACKER_FILE}; }" EXIT
 curl -Lo ${PACKER_FILE} "${PACKER_URI}"
 if [ $? -ne 0 ]; then
   echo "$0: failed to download packer"
-  exit 3
+  exit 4
 fi
-
-PACKER_DIR="${SCRIPTPATH}/packer_bin"
 
 mkdir -p $PACKER_DIR
 $( cd ${PACKER_DIR}; unzip -q $PACKER_FILE )
