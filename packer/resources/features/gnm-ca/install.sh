33f1925a94a77791a9a98e9dd200fe9c1eed4a58
Simon Hildrew <simon.hildrew@theguardian.com>
2015-04-19 22:31:08 +0100

    Add GNM CA feature

---
+++ packer/resources/features/gnm-ca/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script must be run as root
+# Install GNM CA in the OS and JVM - you *probably* only want to do this in
+# pre-prod environments to reduce the impact of the mis-use of the GNM root CA.
+set -e
+
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+CERTIFICATE=${SCRIPTPATH}/GNM-root-cert.pem
+
+# Make sure ca-certificates-java is installed
+if (dpkg -s ca-certificates-java 2> /dev/null > /dev/null); then
+    sudo apt-get install -y ca-certificates-java
+fi
+
+mkdir -p /usr/local/share/ca-certificates/GNM
+cp ${CERTIFICATE} /usr/local/share/ca-certificates/GNM
+
+update-ca-certificates
