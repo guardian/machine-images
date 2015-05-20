40fd6c3bfb9f252f3233e8435bf94220454c0837
Chris Birchall <chris.birchall@guardian.co.uk>
2015-05-20 14:30:05 +0100

    Escape sed input

--- packer/resources/features/logstash/install.sh
+++ packer/resources/features/logstash/install.sh
@@ -16,9 +16,11 @@ CONFIGFILE=${2?"Config file path missing. $USAGE"}
 ln -s /opt/features/logstash/logstash /opt/logstash
 
 # TODO could use the handy substitution script once #23 is merged
+ESCAPED_USER=$(echo $USER | sed -e 's/[\/&]/\\&/g')
+ESCAPED_CONFIGFILE=$(echo $CONFIGFILE | sed -e 's/[\/&]/\\&/g')
 sed \
-  -e 's/@USER@/$USER/g' \
-  -e 's/@CONFIGFILE@/$CONFIGFILE/g' \
+  -e "s/@USER@/$ESCAPED_USER/g" \
+  -e "s/@CONFIGFILE@/$ESCAPED_CONFIGFILE/g" \
   /opt/features/logstash/logstash.conf.template > /etc/init/logstash.conf
 
 echo "Logstash is now installed as an Upstart service. You can start it by running 'sudo start logstash'."
