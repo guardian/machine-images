0ed80c151448e1a2b5a6aa17ca8c96b8ee1e8032
Andy Botting <andy@andybotting.com>
2015-07-27 15:22:06 +1000

    Inital update to support newer ES and Ubuntu Vivid/systemd

--- packer/resources/features/elk-stack/install.sh
+++ packer/resources/features/elk-stack/install.sh
@@ -7,18 +7,17 @@ set -e
 
 FEATURE_ROOT=/opt/features/elk-stack
 
-KIBANA_VERSION=4.1.0
+KIBANA_VERSION=4.1.1
 
 ## Add repositories we are going to use
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-echo "deb http://packages.elastic.co/elasticsearch/1.5/debian stable main" > /etc/apt/sources.list.d/elasticsearch.list
+echo "deb http://packages.elastic.co/elasticsearch/1.7/debian stable main" > /etc/apt/sources.list.d/elasticsearch.list
 echo "deb http://packages.elastic.co/logstash/1.5/debian stable main" > /etc/apt/sources.list.d/logstash.list
 
 ## Update index and install packages
 apt-get update
 apt-get --yes --force-yes install ruby ruby-dev python-pip \
-    golang libwww-perl libdatetime-perl nginx \
-    logstash elasticsearch
+    golang libwww-perl libdatetime-perl nginx logstash elasticsearch
 
 ## Install Cloudwatch monitoring scripts
 wget "http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip" -O /tmp/CloudWatchMonitoringScripts.zip
@@ -32,7 +31,7 @@ crontab -u root - << EOM
 EOM
 
 ## Install Elasticsearch plugins
-/usr/share/elasticsearch/bin/plugin --install elasticsearch/elasticsearch-cloud-aws/2.5.1
+/usr/share/elasticsearch/bin/plugin --install elasticsearch/elasticsearch-cloud-aws/2.7.0
 /usr/share/elasticsearch/bin/plugin --install mobz/elasticsearch-head
 /usr/share/elasticsearch/bin/plugin --install lukas-vlcek/bigdesk
 /usr/share/elasticsearch/bin/plugin --install karmi/elasticsearch-paramedic
@@ -66,6 +65,7 @@ mv /opt/kibana-${KIBANA_VERSION}-linux-x64 /opt/kibana
 useradd -M -r -U -s /bin/false -d /opt/kibana kibana
 mkdir /var/log/kibana
 chown kibana:kibana /var/log/kibana
+cp $FEATURE_ROOT/systemd-kibana4.service /etc/systemd/system/kibana.service
 
 # Install Kibana ES index backup script
 cp $FEATURE_ROOT/kibana-index-backup /etc/cron.daily/kibana-index-backup
@@ -78,21 +78,8 @@ cd /tmp
 wget https://github.com/bitly/oauth2_proxy/releases/download/v2.0/oauth2_proxy-2.0.linux-amd64.go1.4.2.tar.gz
 tar -zxf oauth2_proxy-2.0.linux-amd64.go1.4.2.tar.gz
 mv /tmp/oauth2_proxy-2.0.linux-amd64.go1.4.2/oauth2_proxy /opt/oauth2_proxy/oauth2_proxy
+cp $FEATURE_ROOT/systemd-oauth2-proxy.service /etc/systemd/system/oauth2-proxy.service
+cp $FEATURE_ROOT/sysconfig-oauth2-proxy /etc/default/oauth2-proxy
 
 ## Install NGINX config
 cp $FEATURE_ROOT/nginx-config /etc/nginx/sites-available/default
-
-## Remove existing init.d config for elasticsearch
-rm /etc/init.d/elasticsearch
-rm /etc/init.d/logstash
-rm /etc/init.d/nginx
-update-rc.d elasticsearch remove
-update-rc.d logstash remove
-update-rc.d nginx remove
-
-## Install upstart configuration
-cp $FEATURE_ROOT/upstart-elasticsearch.conf /etc/init/elasticsearch.conf
-cp $FEATURE_ROOT/upstart-logstash.conf /etc/init/logstash.conf
-cp $FEATURE_ROOT/upstart-kibana4.conf /etc/init/kibana.conf
-cp $FEATURE_ROOT/upstart-oauth2-proxy.conf /etc/init/oauth2-proxy.conf
-cp $FEATURE_ROOT/upstart-nginx.conf /etc/init/nginx.conf
