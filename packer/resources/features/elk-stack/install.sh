5b1ae1089c99ef57d570fdbbca3e120c3ba77f92
Andy Botting <andy@andybotting.com>
2015-07-31 16:24:28 +1000

    Update to use Ubuntu Vivid and other fixes

--- packer/resources/features/elk-stack/install.sh
+++ packer/resources/features/elk-stack/install.sh
@@ -6,22 +6,32 @@
 set -e
 
 FEATURE_ROOT=/opt/features/elk-stack
-KIBANA_VERSION=4.1.0
+
+KIBANA_VERSION=4.1.1
 
 ## Add repositories we are going to use
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-echo "deb http://packages.elastic.co/elasticsearch/1.4/debian stable main" > /etc/apt/sources.list.d/elasticsearch.list
+echo "deb http://packages.elastic.co/elasticsearch/1.7/debian stable main" > /etc/apt/sources.list.d/elasticsearch.list
 echo "deb http://packages.elastic.co/logstash/1.5/debian stable main" > /etc/apt/sources.list.d/logstash.list
-add-apt-repository -y ppa:chris-lea/node.js
-sleep 1
 
 ## Update index and install packages
 apt-get update
-apt-get --yes --force-yes install ruby ruby-dev logstash elasticsearch=1.4.4 \
-    nodejs python-pip golang
+apt-get --yes --force-yes install ruby ruby-dev python-pip \
+    golang libwww-perl libdatetime-perl nginx logstash elasticsearch
+
+## Install Cloudwatch monitoring scripts
+wget "http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip" -O /tmp/CloudWatchMonitoringScripts.zip
+unzip /tmp/CloudWatchMonitoringScripts.zip -d /tmp/
+rm /tmp/CloudWatchMonitoringScripts.zip
+mv /tmp/aws-scripts-mon /usr/local/
+
+# Add disk space cron job
+crontab -u root - << EOM
+*/5 * * * * /usr/local/aws-scripts-mon/mon-put-instance-data.pl --disk-space-util --mem-util --disk-path=/ --disk-path=/data --auto-scaling --from-cron
+EOM
 
 ## Install Elasticsearch plugins
-/usr/share/elasticsearch/bin/plugin --install elasticsearch/elasticsearch-cloud-aws/2.4.2
+/usr/share/elasticsearch/bin/plugin --install elasticsearch/elasticsearch-cloud-aws/2.7.0
 /usr/share/elasticsearch/bin/plugin --install mobz/elasticsearch-head
 /usr/share/elasticsearch/bin/plugin --install lukas-vlcek/bigdesk
 /usr/share/elasticsearch/bin/plugin --install karmi/elasticsearch-paramedic
@@ -46,6 +56,8 @@ cp $FEATURE_ROOT/logstash-indexer.conf /etc/logstash/conf.d/logstash-indexer.con
 
 ## Install logstash plugins
 su - logstash -s /bin/sh -c '/opt/logstash/bin/plugin install logstash-input-kinesis'
+rm /etc/init.d/logstash
+cp $FEATURE_ROOT/systemd-logstash.service /etc/systemd/system/logstash.service
 
 ## Install Kibana
 wget https://download.elastic.co/kibana/kibana/kibana-${KIBANA_VERSION}-linux-x64.tar.gz -O /tmp/kibana-${KIBANA_VERSION}-linux-x64.tar.gz
@@ -55,26 +67,21 @@ mv /opt/kibana-${KIBANA_VERSION}-linux-x64 /opt/kibana
 useradd -M -r -U -s /bin/false -d /opt/kibana kibana
 mkdir /var/log/kibana
 chown kibana:kibana /var/log/kibana
+cp $FEATURE_ROOT/systemd-kibana.service /etc/systemd/system/kibana.service
 
 # Install Kibana ES index backup script
-cp $FEATURE_ROOT/kibana-index-backup /etc/cron.daily
+cp $FEATURE_ROOT/kibana-index-backup /etc/cron.daily/kibana-index-backup
+chmod +x /etc/cron.daily/kibana-index-backup
 
 # Install Google Auth Proxy
 mkdir /opt/oauth2_proxy
 useradd -M -r -U -s /bin/false -d /opt/oauth2_proxy oauth2-proxy
 cd /tmp
-wget https://github.com/bitly/oauth2_proxy/releases/download/v1.1.1/google_auth_proxy-1.1.1.linux-amd64.go1.4.2.tar.gz
-tar -zxf google_auth_proxy-1.1.1.linux-amd64.go1.4.2.tar.gz
-mv /tmp/google_auth_proxy-1.1.1.linux-amd64.go1.4.2/google_auth_proxy /opt/oauth2_proxy/oauth2_proxy
+wget https://github.com/bitly/oauth2_proxy/releases/download/v2.0/oauth2_proxy-2.0.linux-amd64.go1.4.2.tar.gz
+tar -zxf oauth2_proxy-2.0.linux-amd64.go1.4.2.tar.gz
+mv /tmp/oauth2_proxy-2.0.linux-amd64.go1.4.2/oauth2_proxy /opt/oauth2_proxy/oauth2_proxy
+cp $FEATURE_ROOT/systemd-oauth2-proxy.service /etc/systemd/system/oauth2-proxy.service
+cp $FEATURE_ROOT/sysconfig-oauth2-proxy /etc/default/oauth2-proxy
 
-## Remove existing init.d config for elasticsearch
-rm /etc/init.d/elasticsearch
-rm /etc/init.d/logstash
-update-rc.d elasticsearch remove
-update-rc.d logstash remove
-
-## Install upstart configuration
-cp $FEATURE_ROOT/upstart-elasticsearch.conf /etc/init/elasticsearch.conf
-cp $FEATURE_ROOT/upstart-logstash.conf /etc/init/logstash.conf
-cp $FEATURE_ROOT/upstart-kibana4.conf /etc/init/kibana.conf
-cp $FEATURE_ROOT/upstart-oauth2-proxy.conf  /etc/init/oauth2-proxy.conf
+## Install NGINX config
+cp $FEATURE_ROOT/nginx-config /etc/nginx/sites-available/default
