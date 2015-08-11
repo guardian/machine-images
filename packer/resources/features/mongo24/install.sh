53cb349f022db83ac4108cb4367283ddea515dba
Simon Hildrew <simon.hildrew@theguardian.com>
2015-08-11 15:03:56 +0100

    For the sake of debugging, let's add rsyslog config

--- packer/resources/features/mongo24/install.sh
+++ packer/resources/features/mongo24/install.sh
@@ -19,3 +19,9 @@ echo "Installing Ruby gems for helper scripts"
 gem install aws-sdk -v '~> 2'
 gem install mongo -v '~> 2'
 gem install bson -v '~> 3'
+
+echo "Installing rsyslog config"
+cat > /etc/rsyslog.d/31-mongo-scripts.conf <<EOF
+local1.*    /var/log/mongodb/scripts.log
+EOF
+systemctl restart rsyslog
