057b1c4f17a33679e440275a15be72ac4d35e1a8
Niklas Lindblad <niklas.lindblad@guardian.co.uk>
2015-11-16 15:01:49 +0000

    Improve permissioning and file ownership

--- packer/resources/features/ssh-keys/install.sh
+++ packer/resources/features/ssh-keys/install.sh
@@ -47,6 +47,35 @@ fi
 
 TEMPORARY_SSH_KEY_FILE="/tmp/authorized_keys.$$"
 
+function ensure_ssh_directory_exists {
+  local TARGET_USER=$1
+  eval TARGET_USER_HOME="~${TARGET_USER}"
+  local TARGET_USER_GROUP=`id -gn ${TARGET_USER}`
+  local TARGET_DIRECTORY="${TARGET_USER_HOME}/.ssh/"
+  local ret=0
+  local CMD="mkdir -p ${TARGET_DIRECTORY}"
+  >&2 echo "Ensuring ${TARGET_DIRECTORY} exists: ${CMD}"
+  RESULT=`${CMD}` || ret=$?
+  if [ ${ret} != 0 ]; then
+    >&2 echo "Error ensuring ${TARGET_DIRECTORY} exists"
+    return 1
+  fi
+  local CMD="chown -R ${TARGET_USER}:${TARGET_USER_GROUP} ${TARGET_DIRECTORY}"
+  >&2 echo "Ensuring ownership for ${TARGET_DIRECTORY}: ${CMD}"
+  RESULT=`${CMD}` || ret=$?
+  if [ ${ret} != 0 ]; then
+    >&2 echo "Error ensuring ownership for ${TARGET_DIRECTORY}"
+    return 1
+  fi
+  local CMD="chmod -R 700 ${TARGET_DIRECTORY}"
+  >&2 echo "Ensuring permissions for ${TARGET_DIRECTORY}: ${CMD}"
+  RESULT=`${CMD}` || ret=$?
+  if [ ${ret} != 0 ]; then
+    >&2 echo "Error ensuring permissions for ${TARGET_DIRECTORY}"
+    return 1
+  fi
+}
+
 function check_ssh_key_url_is_valid {
   local S3_URL=$1
   local ret=0
@@ -91,17 +120,26 @@ function ensure_key_file_permissions {
   local TARGET_USER=$1
   local TEMPORARY_KEY_FILE=$2
   eval TARGET_USER_HOME="~${TARGET_USER}"
+  local TARGET_USER_GROUP=`id -gn ${TARGET_USER}`
   local TARGET_KEY_FILE="${TARGET_USER_HOME}/.ssh/authorized_keys"
   local ret=0
   local CMD="chmod 600 ${TARGET_KEY_FILE}"
   >&2 echo "Ensuring file permissions for key file: ${CMD}"
   RESULT=`${CMD}` || ret=$?
   if [ ${ret} != 0 ]; then
-    >&2 echo "Error ensuring file permissions for key file: ${CMD}"
+    >&2 echo "Error ensuring file permissions for key file"
+    return 1
+  fi
+  local CMD="chown ${TARGET_USER}:${TARGET_USER_GROUP} ${TARGET_KEY_FILE}"
+  >&2 echo "Ensuring file ownership for key file: ${CMD}"
+  RESULT=`${CMD}` || ret=$?
+  if [ ${ret} != 0 ]; then
+    >&2 echo "Error ensuring file ownership for key file"
     return 1
   fi
 }
 
+ensure_ssh_directory_exists "${SSH_USER}"
 check_ssh_key_url_is_valid "${S3_KEY_URL}"
 fetch_ssh_key_from_url "${S3_KEY_URL}" "${TEMPORARY_SSH_KEY_FILE}"
 install_ssh_key_for_user "${SSH_USER}" "${TEMPORARY_SSH_KEY_FILE}"
