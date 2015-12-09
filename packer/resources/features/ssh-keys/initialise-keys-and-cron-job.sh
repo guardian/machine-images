1cadd1e467b9a75fecf9ea4f112bf09e15409a7a
Philip McMahon <philip.mcmahon@guardian.co.uk>
2015-12-09 11:22:09 +0000

    Add script to install keys from local, install from s3, then setup a cron job to regularly install from s3.

---
+++ packer/resources/features/ssh-keys/initialise-keys-and-cron-job.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function HELP {
+>&2 cat << EOF
+
+  Usage: ${0} -t team-github-name -b github-team-keys [-u ubuntu]
+
+  This script runs install-from-local.sh and sets up a cron job for install.sh to regularly
+  update the installed ssh keys
+
+    -u user       [optional] the user to install the SSH keys for. Defaults to ubuntu.
+
+    -t team-name  The name of the team on github to have ssh access.
+
+    -b github-keys-bucket The bucket containing team github keys
+
+    -h            Displays this help message. No further functions are
+                  performed.
+
+EOF
+exit 1
+}
+
+# Process options
+while getopts u:t:b:h FLAG; do
+  case $FLAG in
+    u)
+      SSH_USER=$OPTARG
+      ;;
+    t)
+      GITHUB_TEAM_NAME=$OPTARG
+      ;;
+    b)
+      GITHUB_KEYS_BUCKET=$OPTARG
+      ;;
+    h)  #show help
+      HELP
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${GITHUB_TEAM_NAME}" -o -z "${GITHUB_KEYS_BUCKET}" ]; then
+    echo "Must specify a github team name and bucket"
+    exit 1
+fi
+
+if [ -z "${SSH_USER}" ]; then
+  SSH_USER="ubuntu"
+fi
+
+${DIR}/install-from-local.sh -t ${GITHUB_TEAM_NAME}
+${DIR}/install.sh -t ${GITHUB_TEAM_NAME} -b ${GITHUB_KEYS_BUCKET}
+echo "*/30 * * * * /opt/features/ssh-keys/install.sh -b ${GITHUB_KEYS_BUCKET} -t ${GITHUB_TEAM_NAME}" > ${DIR}/ssh-keys-cron-job.txt
+crontab -u ${SSH_USER} ${DIR}/ssh-keys-cron-job.txt
