af9da93cfcb6e0cdf35a8e37885df24e451b97f0
Niklas Lindblad <niklas.lindblad@guardian.co.uk>
2016-02-08 17:46:05 +0000

    Install from S3 if possible

--- packer/resources/features/mongo24/scripts/install-binaries-only.sh
+++ packer/resources/features/mongo24/scripts/install-binaries-only.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-MONGO_DOWNLOAD_URL_BASE="http://downloads.mongodb.org/linux"
+S3_MIRROR_DOWNLOAD_URL_BASE="https://s3-eu-west-1.amazonaws.com/gu-mongodb-binary-mirror"
+INTERNET_MIRROR_DOWNLOAD_URL_BASE="https://fastdl.mongodb.org/linux"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -42,15 +43,37 @@ if [ -z "${MONGO_VERSION}" ]; then
     exit 1
 fi
 
-MONGO_DOWNLOAD_URL="${MONGO_DOWNLOAD_URL_BASE}/mongodb-linux-x86_64-${MONGO_VERSION}.tgz"
+function url_exists() {
+  local URL=${1}
+  curl --output /dev/null --silent --head --fail "${URL}"
+}
+
+function install_from_url() {
+  local DOWNLOAD_URL=${1}
+  local INSTALL_TARGET=${2}
+  local TEMPORARY_FILE="/tmp/mongodb.tar.gz.$$"
+
+  curl "${DOWNLOAD_URL}" --output "${TEMPORARY_FILE}"
+  mkdir -p "${INSTALL_TARGET}"
+  tar xzf "${TEMPORARY_FILE}" -C "${INSTALL_TARGET}" --strip-components 1
+  rm "${TEMPORARY_FILE}"
+}
+
+S3_MONGO_DOWNLOAD_URL="${S3_MIRROR_DOWNLOAD_URL_BASE}/mongodb-linux-x86_64-${MONGO_VERSION}.tgz"
+INTERNET_MONGO_DOWNLOAD_URL="${INTERNET_MIRROR_DOWNLOAD_URL_BASE}/mongodb-linux-x86_64-${MONGO_VERSION}.tgz"
+
 TEMPORARY_FILE="/tmp/mongodb-${MONGO_VERSION}.tar.gz.$$"
 INSTALL_TARGET="/opt/mongodb/${MONGO_VERSION}"
 
-if curl --output /dev/null --silent --head --fail "${MONGO_DOWNLOAD_URL}"; then
-  curl "${MONGO_DOWNLOAD_URL}" --output "${TEMPORARY_FILE}"
-  mkdir -p "${INSTALL_TARGET}"
-  tar xzf "${TEMPORARY_FILE}" -C "${INSTALL_TARGET}" --strip-components 1
+# Try installing from S3 first
+if url_exists "${S3_MONGO_DOWNLOAD_URL}" ; then
+  install_from_url "${S3_MONGO_DOWNLOAD_URL}" "${INSTALL_TARGET}"
 else
-  echo "URL does not exist: ${MONGO_DOWNLOAD_URL}"
-  exit 1
+  echo "Could not find MongoDB ${MONGO_VERSION} binaries in S3 mirror. Fetching from official MongoDB mirror."
+  if url_exists "${INTERNET_MONGO_DOWNLOAD_URL}"; then
+    install_from_url "${INTERNET_MONGO_DOWNLOAD_URL}" "${INSTALL_TARGET}"
+  else
+    echo "URL does not exist: ${INTERNET_MONGO_DOWNLOAD_URL}"
+    exit 1
+  fi
 fi
