c62ced2116cedd2b9017664d463f53c8017b2a6b
Niklas Lindblad <niklas.lindblad@guardian.co.uk>
2016-02-12 13:43:56 +0000

    Do not exit when mongod cannot start up

--- packer/resources/features/mongo-opsmanager/server-install.sh
+++ packer/resources/features/mongo-opsmanager/server-install.sh
@@ -9,7 +9,7 @@ SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 apt-get update
-apt-get install -y mongodb-org
+apt-get install -y mongodb-org || true
 
 # disable default mongod and delete any created files
 service mongod stop
