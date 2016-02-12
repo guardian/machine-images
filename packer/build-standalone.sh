9640f62916580c307d161a71710d378685729f43
Niklas Lindblad <niklas.lindblad@guardian.co.uk>
2016-02-12 10:52:14 +0000

    Fix linebreak

--- packer/build-standalone.sh
+++ packer/build-standalone.sh
@@ -10,5 +10,5 @@ echo "Running packer with ${packer_file}" 1>&2
 ${PACKER_HOME}/packer build $FLAGS \
   -var "build_name=${build_name}" \
   -var "aws_access_key=${AWS_ACCESS_KEY_ID}" \
-  -var "aws_access_key=${AWS_SECRET_ACCESS_KEY}"
+  -var "aws_access_key=${AWS_SECRET_ACCESS_KEY}" \
   ${packer_file}
